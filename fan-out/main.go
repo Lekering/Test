e6059a7f840c635f2d98c6ff7a96116486c9f519
Grisha <grishhasemynov@gmail.com>
2025-12-19 14:54:24 +0300

    New_patterns

--- fan-out/main.go
+++ fan-out/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
@@ -19,26 +18,22 @@ func main() {
 
 	outChans := SplitChan(in, 2)
 
-	for i, out := range outChans {
+	for _, out := range outChans {
+
 		wg.Add(1)
-		sleepDuration := time.Millisecond * 200
-		if i == 1 {
-			sleepDuration = time.Millisecond * 100
-		}
-		go func(out <-chan int, sleep time.Duration) {
+
+		go func(out <-chan int) {
 			defer wg.Done()
 			for v := range out {
-				time.Sleep(sleep)
 				fmt.Println(v)
 			}
-		}(out, sleepDuration)
+		}(out)
 	}
 
 	wg.Wait()
 }
 
 func SplitChan[T any](in <-chan T, n int) []chan T {
-	wg := &sync.WaitGroup{}
 
 	outChans := make([]chan T, n)
 	for i := range outChans {
@@ -47,23 +42,15 @@ func SplitChan[T any](in <-chan T, n int) []chan T {
 
 	go func() {
 		defer func() {
-			wg.Wait() // ждем завершения всех горутин перед закрытием каналов
 			for _, c := range outChans {
 				close(c)
 			}
 		}()
-		for v := range in {
-			for _, c := range outChans {
-				select {
-				case c <- v:
-				default: // prevent goroutine block if receiver is slow
-					wg.Add(1)
-					go func(val T, ch chan T) {
-						defer wg.Done()
-						ch <- val
-					}(v, c)
-				}
-			}
+
+		ind := 0
+		for value := range in {
+			outChans[ind] <- value
+			ind = (ind + 1) % n
 		}
 	}()
 
