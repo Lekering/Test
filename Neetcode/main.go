8057874b8404225cfdc5abf09403c5b122b05f4a
Grisha <grishhasemynov@gmail.com>
2026-01-09 19:35:38 +0300

    Update main.go

--- Neetcode/main.go
+++ Neetcode/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	fmt.Println("HI")
+
+	wg := &sync.WaitGroup{}
+	ch := make(chan int, 20) // буферизованный канал на 20 элементов
+	for i := range 20 {
+		ch <- i
+	}
+	close(ch)
+	wg.Add(2)
+	go consumer(wg, ch)
+	go consumer(wg, ch)
+	wg.Wait()
+}
+
+func Revers(array []int) {
+	f, s := 0, len(array)-1
+
+	for f < s {
+		array[f], array[s] = array[s], array[f]
+		f++
+		s--
+	}
+	fmt.Println(array)
+}
+
+func consumer(wg *sync.WaitGroup, ch chan int) {
+	defer wg.Done()
+	for v := range ch {
+		fmt.Println(v)
+	}
 }
